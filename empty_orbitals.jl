1b37eabc5a4f96994a91a77d8843ddcf5e4bb2f9
hq-tr <hqt210@gmail.com>
2026-08-06 15:30:11 +0800

    added option to work on basis only

--- empty_orbitals.jl
+++ empty_orbitals.jl
@@ -13,6 +13,7 @@ function main()
 		@argumentflag decimal "--decimal"
 		@argumentoptional Int No "--n_orb" "-o"
 		@argumentflag nonormalization "--no-normalization"
+		@argumentflag basisonly "--basis-only"
 	end
 
 	# Validate input
@@ -23,30 +24,50 @@ function main()
 
 	@assert k > 0 "Number of empty orbitals 'n_empty' must be a positive integer"
 
+	if basisonly 
+		decimal = true
+		nonormalization = true
+	end
+
 	if decimal && No == nothing
 		println("For input file of decimal format, the number of orbitals must be specified.")
 		return
 	end
 
 	# Read file
-	if decimal
+	if basisonly
+		state = readbasis(filename,No)
+	elseif decimal
 		state = readwfdec(filename,No)
 	else
 		state = readwf(filename)
 	end
-	new_coefs = Float64[]
-	new_basis = BitVector[]
 
-	for (basis,coef) in zip(state.basis,state.coef)
-		if count(basis[1:k]) == 0
-			push!(new_basis,basis)
-			push!(new_coefs,coef)
+
+
+	if basisonly
+		new_basis = BitVector[]
+		for basis in state.basis
+			if count(basis[1:k]) == 0
+				push!(new_basis,basis)
+			end
+		end
+		new_state = FQH_state(new_basis)
+	else
+		new_coefs = Float64[]
+		new_basis = BitVector[]
+		for (basis,coef) in zip(state.basis,state.coef)
+			if count(basis[1:k]) == 0
+				push!(new_basis,basis)
+				push!(new_coefs,coef)
+			end
+		end
+		if !nonormalization
+			new_state = wfnormalize(FQH_state(new_basis,new_coefs))
 		end
 	end
 
-	if !nonormalization
-		new_state = wfnormalize(FQH_state(new_basis,new_coefs))
-	end
+	
 	printwf(new_state;fname="$(filename)_empty_k_$(k)")
 end
 
